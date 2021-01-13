12cf09b070fb7f5d15967a66940a29eb38a0077c
Ozzie Harb <ozharb@gmail.com>
2021-01-12 19:04:34 -0800

    update database_url variable

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL ||'postgresql://postgres@localhost/noteful_test'
 };
