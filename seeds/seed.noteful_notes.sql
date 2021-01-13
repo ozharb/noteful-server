d83d0144c89934de435157570612616d0c540aff
Ozzie Harb <ozharb@gmail.com>
2021-01-13 11:29:50 -0800

    minor update to seed file

--- seeds/seed.noteful_notes.sql
+++ seeds/seed.noteful_notes.sql
@@ -1,16 +1,16 @@
 INSERT INTO noteful_notes (notename, content, folder_id)
 VALUES
 
-('porsche', '1982',1),
-('Ferrari 288 GTO', '1984',1),
-('Lamborghini Countach', '1982',1),
-('Peugeot 205', '1983',1),
-('Tron', '1982',2),
-('Risky Business', '1983',2),
-('Body Double', '1984',2),
-('Duran Duran', '1983',3),
-('Speak & Spell', '1981',3),
-('Synchronicity', '1984',3),
-('Robotron: 2084', '1982',4),
-('Cosmo Police Galivan', '1985',4),
-('Racing Destruction Set', '1985',4);
+('porsche', '1982',9),
+('Ferrari 288 GTO', '1984',9),
+('Lamborghini Countach', '1982',9),
+('Peugeot 205', '1983',9),
+('Tron', '1982',10),
+('Risky Business', '1983',10),
+('Body Double', '1984',10),
+('Duran Duran', '1983',11),
+('Speak & Spell', '1981',11),
+('Synchronicity', '1984',11),
+('Robotron: 2084', '1982',12),
+('Cosmo Police Galivan', '1985',12),
+('Racing Destruction Set', '1985',12);
